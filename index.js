e6f6554a352484b27298dec93b5d570fae804d48
Hope Kathomi <kathomihope@gmail.com>
2026-07-15 10:44:09 +0300

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+const credentials = {
+    "username": "user1",
+    "password": "password123"
+}
 
+function createLoginTracker(userInfo){
+  let attemptCount = 0;
 
+  let countAttempts = (passwordAttempt) =>{
+    passwordAttempt.forEach(pswd => {
+      attemptCount++;
+       if(pswd === userInfo.password && attemptCount <= 3){
+        console.log("Login successful");
+        return;
+      }else if (pswd!== userInfo.password && attemptCount <=3){
+         console.log("Login failed");
+      } else{
+        console.log("Account locked due to too many failed login attempts");
+      }
+    });
+   
+  }
+  countAttempts(["123","1245","abc", "password123"])
+}
+
+createLoginTracker(credentials)
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
